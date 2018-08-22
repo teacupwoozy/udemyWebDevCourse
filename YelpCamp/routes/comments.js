3bf866d0b21c07e84d567b840f57ef2489b88138
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-08-21 20:30:59 -0500

    daily progress on yelpcamp project

---
+++ YelpCamp/routes/comments.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// Comments New
+router.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+       } else {
+            res.render("comments/new", {campground: campground});
+       }
+    });
+});
+
+// Comments Create
+router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
+    // Lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/camgrounds");
+       } else {
+          Comment.create(req.body.comment, function(err, comment){
+             if(err){
+                 console.log(err);
+             } else {
+                //  Add username and id to comment
+                comment.author.id = req.user._id;
+                comment.author.username = req.user.username;
+                // Save comment
+                 comment.save();
+                 campground.comments.push(comment);
+                 campground.save();
+                 res.redirect("/campgrounds/" + campground._id);
+             }
+          });
+       }
+    });
+});
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
